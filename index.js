1dcd8f62a7d6471bd91489eac5c1004e41d2c382
mahdi849 <afg078237@gmail.com>
2026-01-03 06:58:51 +0430

    oo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const conversations = new Map();
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 conversations.get(chatId)
